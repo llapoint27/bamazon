7495093f2bc4a3779162eb4a4d473c350e4bf3d5
Marco Polo <laurenlapoint@Laurens-MacBook-Pro.local>
2018-09-24 15:32:51 -0400

    updated password security

--- bamazon.js
+++ bamazon.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table");
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "LaL@1991",
+    password: process.env.MYSQL_KEY,
     database: "bamazon_DB"
 });
 
